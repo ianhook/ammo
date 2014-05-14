cd97796b30a75be26948391e18375477e0be69fa
Ian Hook <ianhook@gmail.com>
2014-05-14 02:49:53 +0000

    fix example secret file

--- secret.js.ex
+++ secret.js.ex
@@ -1,9 +1,35 @@
-exports.data = {
+var data = {
     'domain' : '',
     'oauth': { secret: '', public:''},
     'analytics': '',
     'facebook': {'id': 1, secret:''},
+    'googleapi': {id: '' },
     'soundcloud': {id:'', secret:''},
     'youtube': {id:'not needed?'},
-}
+};
 
+/********** STOP CHANGING **************/
+exports.data = data;
+exports.safe = function(req, res) {
+    //console.log("Sending safe");
+    var safe = {}, tmp;
+    for ( key in data) {
+        if(typeof(data[key]) == 'object'){
+            tmp = {};
+            for ( prop in data[key]) {
+                if(prop != 'secret') {
+                    tmp[prop] = data[key][prop];
+                }
+            }
+            safe[key] = tmp;
+        } else {
+            safe[key] = data[key];
+        }
+    }
+
+    //res.set('Content-Type', 'application/x-javascript');
+    res.send("angular.module('ammoApp.config',[]).constant('ammoConfig'," +
+            JSON.stringify(safe)
+            + ");");
+    return safe;
+};
